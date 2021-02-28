317ef7541c72a3dda34822d78425122bca15f4e8
sugianto43 <antho9922@gmail.com>
2021-02-28 14:28:04 +0800

    fetch API from local JSON file

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
-const scoreText = document.getElementById('score');
-const progressBarFull = document.getElementById('progressBarFull');
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -10,95 +10,81 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: 'Inside which HTML element do we put Javascript?',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1
-    },
-    {
-        question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-        choice1: '<script href="xxx.js">',
-        choice2: '<script name="xxx.js">',
-        choice3: '<script src="xxx.js">',
-        choice4: '<script file="xxx.js">',
-        answer: 3
-    },
-    {
-        question: 'How do you write "Hello World" in an alert box?',
-        choice1: 'msgBox("Hello World")',
-        choice2: 'alertBox("Hello World")',
-        choice3: 'msg("Hello World")',
-        choice4: 'alert("Hello World")',
-        answer: 4
-    }
-]
+let questions = [];
+
+fetch("questions.json")
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
+    startGame();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = questions.length;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
 };
 
 getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end of the page
+    return window.location.assign("/end.html");
+  }
 
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
-        //go to the end of the page
-        return window.location.assign('/end.html');
-    }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  // update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    // update the progress bar
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if (!acceptingAnswers) return;
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const seletedAnswer = selectedChoice.dataset['number'];
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const seletedAnswer = selectedChoice.dataset["number"];
 
-        const classToApply = seletedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+    const classToApply =
+      seletedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        if (classToApply === 'correct') {
-            incrementScore(CORRECT_BONUS);
-        }
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
 
-        selectedChoice.parentElement.classList.add(classToApply);
-        setTimeout( () => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000)
-    });
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
 });
 
-incrementScore = num => {
-    score += num;
-    scoreText.innerText = score;
-}
-
-startGame();
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
--- highscores.js
+++ highscores.js
@@ -1,7 +1,9 @@
-const highScoresList = document.getElementById('highScoresList');
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScoresList.innerHTML = highScores.map(score => {
+highScoresList.innerHTML = highScores
+  .map((score) => {
     return `<li class='high-score'>${score.name} - ${score.score}</li>`;
-}).join('');
-console.log(highScoresList)
+  })
+  .join("");
+console.log(highScoresList);
